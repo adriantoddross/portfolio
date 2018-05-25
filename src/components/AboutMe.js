c6ddf332a48cef156b066ec23da65aad322416e4
Adrian <adriantoddross@gmail.com>
2018-05-25 12:27:57 -0400

    finish about me copy

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,5 +1,20 @@
 import React from 'react';
 
 export default function AboutMe(props) {
-  return();
+  return(
+    <section>
+      <h1>
+        Yo Adrian, I did it!
+      </h1>
+      <p>
+        Hey there! I'm Adrian, a full stack developer and graphic designer living in Atlanta, Georgia.
+
+        I have experience building and deploying web apps and working collaboratively through pair programming and alongside project teams. I mostly work with NodeJS and React JS, using tools like Travis CI, Heroku and Netlify for testing and deployment. I care a lot about accessibility, semantic code, and user experience.
+
+        I spend my free time playing video games, listening to podcasts, and enjoying the company of my wife (and my fur babies!). I dabbled in a lot of creative hobbies like audio engineering, recording & producing music, streaming video games. If I'm not laughing, I'm trying to figure out
+
+        Currently, I'm looking for employment and freelance opportunities. Feel free to email me.
+      </p>
+    </section>
+  );
 }
