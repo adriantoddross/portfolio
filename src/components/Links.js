7ada2978e374d2e1198bb606ed43e7dab37f50f2
adriantoddross <adriantoddross@gmail.com>
2018-05-28 16:31:12 -0400

    create component for links

---
+++ src/components/Links.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Links(props) {
+  return(
+    <div>
+      <ul>
+        <li>
+          <a href="http://">Github</a>
+        </li>
+        <li>
+          <a href="http://">LinkedIn</a>
+        </li>
+      </ul>
+    </div>
+  );
+}
