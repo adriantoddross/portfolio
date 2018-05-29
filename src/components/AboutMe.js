746398199f89b2f782cd63bf37b7cbf5fad5d250
Adrian <adriantoddross@gmail.com>
2018-05-29 12:26:34 -0400

    add space between paragraphs with p tags

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -4,17 +4,18 @@ export default function AboutMe(props) {
   return (
     <section>
       <h1>
-        Yo Adrian, I did it!
+        I eat crunchy peanut butter out of the jar.
       </h1>
       <p>
-        Hey there! I'm Adrian, a full stack developer and graphic designer living in Atlanta, Georgia.
-
+        Hey there! I'm <b>Adrian</b>, a full stack developer and graphic designer living in Atlanta, Georgia.
+      </p>
+      <p>
         I have experience building and deploying web apps and working collaboratively through pair programming and alongside project teams. I mostly work with NodeJS and React JS, using tools like Travis CI, Heroku and Netlify for testing and deployment. I care a lot about accessibility, semantic code, and user experience.
 
         I spend my free time playing video games, listening to podcasts, and enjoying the company of my wife (and my fur babies!). I dabble in a lot of creative hobbies like audio engineering, recording & producing music, and streaming video games.
-
-        Currently, I'm looking for a full-time job. Feel free to email me with any opportunties.
       </p>
+
+      <p>Currently, I'm looking for a full-time job. Feel free to email me with any opportunties.</p>
     </section>
   );
 }
