5036d69c6c0e1a63c334d5060afb2c583978a908
Adrian <adriantoddross@gmail.com>
2018-05-29 15:51:48 -0400

    import app css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
 import Portfolio from './components/Portfolio';
+import './App.css';
 class App extends Component {
   render() {
     return (
