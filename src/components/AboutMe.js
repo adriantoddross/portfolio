f0f8dcfb7813366e8b030ed23aa16f1bb5f77a18
Adrian <adriantoddross@gmail.com>
2018-05-29 11:57:17 -0400

    revise about me

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -11,9 +11,9 @@ export default function AboutMe(props) {
 
         I have experience building and deploying web apps and working collaboratively through pair programming and alongside project teams. I mostly work with NodeJS and React JS, using tools like Travis CI, Heroku and Netlify for testing and deployment. I care a lot about accessibility, semantic code, and user experience.
 
-        I spend my free time playing video games, listening to podcasts, and enjoying the company of my wife (and my fur babies!). I dabbled in a lot of creative hobbies like audio engineering, recording & producing music, streaming video games. If I'm not laughing, I'm trying to figure out
+        I spend my free time playing video games, listening to podcasts, and enjoying the company of my wife (and my fur babies!). I dabble in a lot of creative hobbies like audio engineering, recording & producing music, and streaming video games.
 
-        Currently, I'm looking for employment and freelance opportunities. Feel free to email me.
+        Currently, I'm looking for a full-time job. Feel free to email me with any opportunties.
       </p>
     </section>
   );
