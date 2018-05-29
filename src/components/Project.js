df6348b39e52d330dd1696995f3ce8fc1a5925ac
Adrian <adriantoddross@gmail.com>
2018-05-29 15:19:26 -0400

    replace h1 with h3 tag

--- src/components/Project.js
+++ src/components/Project.js
@@ -6,7 +6,7 @@ export default function Project(props) {
       <div>
         <img src={props.image} alt=""/>
       </div>
-      <h1>{props.title}</h1>
+      <h3>{props.title}</h3>
       <p>{props.description}</p>
       <a href={props.link}>Live Demo</a>
     </div>
