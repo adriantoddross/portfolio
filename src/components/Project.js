3f0b0b525b41323cc5eecf9b7480b89e7bb86feb
Adrian <adriantoddross@gmail.com>
2018-05-29 11:17:25 -0400

    add props for projects

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,7 +3,12 @@ import React from 'react';
 export default function Project(props) {
   return (
     <div>
-      
+      <div>
+        <img src={props.image} alt=""/>
+      </div>
+      <h1>{props.title}</h1>
+      <p>{props.description}</p>
+      <a href={props.link}>Live Demo</a>
     </div>
   );
 }
