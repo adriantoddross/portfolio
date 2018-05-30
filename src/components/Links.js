a06df4076a85f0e4e0f0ffc28a3e861172dde842
Adrian <adriantoddross@gmail.com>
2018-05-30 15:37:58 -0400

    add classname for div

--- src/components/Links.js
+++ src/components/Links.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Links(props) {
   return(
-    <div>
+    <div className='links-container'>
       <ul className="links">
         <li>
           <a className='github' href="http://https://github.com/adriantoddross" target="_blank" rel="noopener noreferrer">Github</a>
