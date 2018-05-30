2ed52d0ae8d18fb2c805e13b3f4f39a4e72a782d
Adrian <adriantoddross@gmail.com>
2018-05-30 16:54:37 -0400

    rename key to project title

--- src/components/List.js
+++ src/components/List.js
@@ -5,7 +5,7 @@ import Portfolio from '../Projects';
 export default function List(props) {
 
   const projects = Portfolio.map(project =>
-    <li key={project.link} className='project'>
+    <li key={project.title} className='project'>
       <Project image={project.image} title={project.title} description={project.description} link={project.link}/>
     </li>
   );
