87bb00e0b9d03992e02ea13ed9f87e806de5333f
Adrian <adriantoddross@gmail.com>
2018-05-29 20:58:22 -0400

    comment out free time paragraph

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -9,11 +9,11 @@ export default function AboutMe(props) {
       <p>
         I have experience building and deploying web apps and working collaboratively through pair programming and alongside project teams. I mostly work with NodeJS and React JS, using tools like Travis CI, Heroku and Netlify for testing and deployment. I care a lot about accessibility, semantic code, and user experience.
       </p>
-      <p>
+      {/* <p>
         I spend my free time playing video games, listening to podcasts, and enjoying the company of my wife (and my fur babies!). I dabble in a lot of creative hobbies like audio engineering, recording & producing music, and streaming video games.
-      </p>
+      </p> */}
       <p>
-        Currently, I'm looking for a full-time job. Feel free to email me with any opportunties.
+        Currently, I'm looking for a full-time job. Feel free to <a href="mailto:adriantoddross@gmail.com">email me</a> with any opportunties.
       </p>
     </section>
   );
