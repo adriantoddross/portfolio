a2dfc5b5074b6bf0c331ee47f1554456ae54b21f
Adrian <adriantoddross@gmail.com>
2018-05-30 15:39:11 -0400

    comment out images

--- src/Projects.js
+++ src/Projects.js
@@ -1,12 +1,12 @@
 const projects = [
   {
-    image: 'https://res.cloudinary.com/adriantoddross/image/upload/v1527625925/SafeR%20Preview.png',
-    title: 'SafeR',
+    // image: 'https://res.cloudinary.com/adriantoddross/image/upload/v1527625925/SafeR%20Preview.png',
+    // title: 'SafeR',
     description: 'View and report local incidents in your community.',
     link: 'https://safer.netlify.com/map'
   },
   {
-    image: 'https://res.cloudinary.com/adriantoddross/image/upload/v1527625925/Simlify%20Preview.png',
+    // image: 'https://res.cloudinary.com/adriantoddross/image/upload/v1527625925/Simlify%20Preview.png',
     title: 'Simlify',
     description: 'Learn Simlish, a fictional language spoken by characters in EA Games\' video games series "The Sims", through spaced repetition.',
     link: 'https://keen-jang-ca814b.netlify.com/'
