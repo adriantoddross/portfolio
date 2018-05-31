022a4aa49ef9bd87f48465cbbb2b1ee707fc587f
Adrian <adriantoddross@gmail.com>
2018-05-31 18:29:26 -0400

    revise contact paragraph

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -5,16 +5,16 @@ export default function AboutMe(props) {
     <div>
         <section className='about'>
           <p className='intro'>
-            Hey there! I'm <b>Adrian</b>, a full stack developer and graphic designer living in Atlanta, Georgia.
+            Hi! I'm <b>Adrian</b>, a full stack developer and graphic designer living in Atlanta, Georgia.
           </p>
           <p className='experience'>
-            I have experience building and deploying web apps and working collaboratively through pair programming and alongside project teams. I mostly work with NodeJS and React JS, using tools like Travis CI, Heroku and Netlify for testing and deployment. I care a lot about accessibility, semantic code, and user experience.
+            I have experience building and deploying web apps and working collaboratively through pair programming and as part of a team. I mostly work with NodeJS and React JS, using tools like Travis CI, Heroku and Netlify for deployment. I care a lot about accessibility, semantic code, and user experience.
           </p>
           {/* <p>
             I spend my free time playing video games, listening to podcasts, and enjoying the company of my wife (and my fur babies!). I dabble in a lot of creative hobbies like audio engineering, recording & producing music, and streaming video games.
           </p> */}
           <p className='contact'>
-            Currently, I'm looking for a full-time job. Feel free to <a
+            Currently, I'm looking for a full-time job and I'd love the chance to work on a team with diverse, communicative developers. Feel free to <a
             className='email' href="mailto:adriantoddross@gmail.com">email me</a> with any opportunties.
           </p>
         </section>
