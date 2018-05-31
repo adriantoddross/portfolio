b81705448c25da6f5bbd05eca70cd3be12330eba
Adrian <adriantoddross@gmail.com>
2018-05-31 11:50:43 -0400

    fix github link

--- src/components/Links.js
+++ src/components/Links.js
@@ -5,7 +5,7 @@ export default function Links(props) {
     <div className='links-container'>
       <ul className="links">
         <li>
-          <a className='github' href="http://https://github.com/adriantoddross" target="_blank" rel="noopener noreferrer">Github</a>
+          <a className='github' href="https://github.com/adriantoddross" target="_blank" rel="noopener noreferrer">Github</a>
         </li>
         <li>
           <a className='linkedin' href="https://www.linkedin.com/in/adriantross/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
