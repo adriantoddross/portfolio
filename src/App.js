5e982a662bf4172cb40f61f05e27a49f2c39d15d
Adrian <adriantoddross@gmail.com>
2018-05-31 12:54:41 -0400

    add footer and attribution

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
 import Portfolio from './components/Portfolio';
+import Footer from './components/Footer';
 import './App.css';
 class App extends Component {
   render() {
@@ -8,6 +9,7 @@ class App extends Component {
       <div className="App">
         <Header/>
         <Portfolio/>
+        <Footer/>
       </div>
     );
   }
