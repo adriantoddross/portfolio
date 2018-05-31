80e23f89c09d79f7b0f2fd9ff163a907e577ae66
Adrian <adriantoddross@gmail.com>
2018-05-31 17:22:49 -0400

    add addtl info

--- src/Projects.js
+++ src/Projects.js
@@ -3,14 +3,14 @@ const projects = [
     image: 'https://res.cloudinary.com/adriantoddross/image/upload/v1527625925/SafeR%20Preview.png',
     title: 'SafeR',
     description: 'View and report local incidents in your community. Built with Google Maps & Places APIs.',
-    stack: 'React JS, React Native, Redux, MongoDB, Travis CI, Heroku, Netlify',
+    stack: 'Node JS, React JS, React Native, Redux JS, MongoDB, Travis CI, Heroku, Netlify',
     repo: 'https://github.com/thinkful-ei18/maajac-client',
     link: 'https://safer.netlify.com/map'
   },
   {
     image: 'https://res.cloudinary.com/adriantoddross/image/upload/v1527625925/Simlify%20Preview.png',
     title: 'Simlify',
-    stack: 'React JS, Spaced Repetition Algorithm, Redux, MongoDB, Travis CI, Heroku, Netlify',
+    stack: 'Singly Linked List (for spaced repetition algorithm), Node JS, React JS, Redux JS, MongoDB, Travis CI, Heroku, Netlify',
     description: 'Learn Simlish, a fictional language spoken by characters in EA Games\' video games series "The Sims", through spaced repetition.',
     repo: 'https://github.com/thinkful-ei18/simlify_client',
     link: 'https://keen-jang-ca814b.netlify.com/'
