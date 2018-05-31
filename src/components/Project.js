800e1a298d81759c29486896fa3f685d0879a6f0
Adrian <adriantoddross@gmail.com>
2018-05-31 14:01:43 -0400

    add props for repo link and stack

--- src/components/Project.js
+++ src/components/Project.js
@@ -7,15 +7,18 @@ export default function Project(props) {
         <img className='project-image' src={props.image} alt="null"/>
       </div>
       <h2 className='project-title'>{props.title}</h2>
+      <p className='project-stack'>{props.stack}</p>
       <p className='project-desc'>{props.description}</p>
+      <a className='project-repo' href={props.repo}>Github Repo</a>
       <a className='project-link' href={props.link}>Live Demo</a>
     </div>
   );
 }
 
 Project.defaultProps = {
-  image: '',
-  title: 'Project coming soon!',
-  description: "This project isn't finished yet, but will be added soon!",
-  link: ''
+  image: 'https://res.cloudinary.com/adriantoddross/image/upload/v1526329107/100px_map_icon_traffic_construction.png',
+  title: 'Under Construction',
+  description: "This project isn't finished yet but it'll be added soon!",
+  repo: 'https://github.com/adriantoddross',
+  link: 'https://github.com/adriantoddross'
 }
