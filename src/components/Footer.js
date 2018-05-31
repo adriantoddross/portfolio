f17eb8980fa3d6ca889bd7fa992f5728eab5d532
Adrian <adriantoddross@gmail.com>
2018-05-31 13:14:29 -0400

    Add MIT license info

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,10 +8,10 @@ export default function Footer(props) {
           <img className='footer-img' src="https://res.cloudinary.com/adriantoddross/image/upload/v1527785141/adrian-ross.png" alt="null"/>
         </div>
         <p>
-          Designed and developed by Adrian, <span className='attribution'>inspired by Pranesh Ravi. <a href="https://praneshravi.in/" target="_blank" rel="noopener noreferrer"></a></span>
+          Designed and developed by Adrian, <span className='attribution'>inspired by <a className='attribution' href="https://praneshravi.in/" target="_blank" rel="noopener noreferrer">Pranesh Ravi</a>.</span>
         </p>
         <p>
-          Hosted on Github Pages.
+          Hosted on Github Pages. Code is licensed under MIT and available on Github. All images and graphics are licensed under CC BY-SA.
         </p>
         <p>
           © 2018 Adrian Ross
