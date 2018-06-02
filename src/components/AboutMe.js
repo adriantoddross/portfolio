0c92288dd668054a91f12ed1201924b8c96b74ea
adriantoddross <adriantoddross@gmail.com>
2018-06-02 17:51:29 -0400

    add h1 tag

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -3,6 +3,7 @@ import React from 'react';
 export default function AboutMe(props) {
   return (
     <div>
+        <h1 className='section-header'>Adrian Ross: Full Stack Web Dev & Graphic Designer</h1>      
         <section className='about'>
           <h2 className='section-header'>About Me</h2>
           <p className='intro'>
