d93b18221da3cb09ae462fed11fb233f1e418515
Adrian <adriantoddross@gmail.com>
2018-06-07 02:50:30 -0400

    add windfall gif

--- src/Projects.js
+++ src/Projects.js
@@ -3,23 +3,23 @@ const projects = [
     image: 'https://res.cloudinary.com/adriantoddross/image/upload/v1527625925/SafeR%20Preview.png',
     title: 'SafeR',
     description: 'View and report local incidents in your community. Built with Google Maps & Places APIs.',
-    stack: 'Express.js, React JS, React Native, Redux JS, MongoDB, Travis CI, Heroku, Netlify',
+    stack: 'Express.js, React JS, React Native, React Redux, Redux Forms, MongoDB, Mongoose.js, Travis CI, Heroku, Netlify',
     repo: 'https://github.com/thinkful-ei18/maajac-client',
     link: 'https://safer.netlify.com/'
   },
   {
     image: 'https://res.cloudinary.com/adriantoddross/image/upload/v1527625925/Simlify%20Preview.png',
     title: 'Simlify',
-    stack: 'Singly Linked List (for spaced repetition algorithm), Express.js, React JS, Redux JS, MongoDB, Heroku, Netlify',
+    stack: 'Singly Linked List (for spaced repetition algorithm), Express.js, React JS, React Redux, Redux Forms, MongoDB, Mongoose.js, Heroku, Netlify',
     description: 'Learn Simlish, a fictional language spoken by characters in EA Games\' video games series "The Sims", through spaced repetition.',
     repo: 'https://github.com/thinkful-ei18/simlify_client',
     link: 'https://keen-jang-ca814b.netlify.com/'
   },
   {
-    image: 'https://res.cloudinary.com/adriantoddross/image/upload/v1528221539/Coming_Soon_Preview.png',
+    image: 'https://res.cloudinary.com/adriantoddross/image/upload/v1528352170/windfall-preview.gif',
     title: 'Windfall',
-    stack: 'Express.js, React JS, Redux JS, PostgresQL, Heroku, Netlify',
-    description: 'A simplified budgeting app created to help you keep track of which bills you\'ve paid.',
+    stack: 'Express.js, React JS, React Redux, Redux Forms, PostgresQL, knex.js, Heroku, Netlify',
+    description: 'Create a list of bills and see your net income.',
     repo: 'https://github.com/thinkful-ei18/adrian-budget-client',
     link: 'http://adrian-budget-app.netlify.com/'
   }
