99db0942efae426d2305f40f6f92765707e653fe
Adrian <adriantoddross@gmail.com>
2018-06-12 18:37:06 -0400

    Revise project descriptions and update image URLs

--- src/Projects.js
+++ src/Projects.js
@@ -1,25 +1,25 @@
 const projects = [
   {
-    image: 'https://res.cloudinary.com/adriantoddross/image/upload/v1527625925/SafeR%20Preview.png',
+    image: 'https://res.cloudinary.com/adriantoddross/image/upload/v1528837857/safeR_Browser_Window.png',
     title: 'SafeR',
-    description: 'View and report local incidents in your community. Built with Google Maps & Places APIs.',
+    description: 'Our team of five created a neighborhood oriented social networking app to view and report local incidents in your community. We used Google Maps & Places APIs and also created a React Native app alongeside our web app.',
     stack: 'Express.js, React JS, React Native, React Redux, Redux Forms, MongoDB, Mongoose.js, Travis CI, Heroku, Netlify',
     repo: 'https://github.com/thinkful-ei18/maajac-client',
     link: 'https://safer.netlify.com/'
   },
   {
-    image: 'https://res.cloudinary.com/adriantoddross/image/upload/v1527625925/Simlify%20Preview.png',
+    image: 'https://res.cloudinary.com/adriantoddross/image/upload/v1528838204/Simlify_Browser_Window.png',
     title: 'Simlify',
     stack: 'Singly Linked List (for spaced repetition algorithm), Express.js, React JS, React Redux, Redux Forms, MongoDB, Mongoose.js, Heroku, Netlify',
-    description: 'Learn Simlish, a fictional language spoken by characters in EA Games\' video games series "The Sims", through spaced repetition.',
+    description: 'Working with a partner, we re-created a singly linked list to use with MongoDB and implemented spaced repetition to teach Simlish, a fictional language featured in EA Games\' The Sims franchise.',
     repo: 'https://github.com/thinkful-ei18/simlify_client',
     link: 'https://keen-jang-ca814b.netlify.com/'
   },
   {
-    image: 'https://res.cloudinary.com/adriantoddross/image/upload/v1527625925/Simlify%20Preview.png',
+    image: 'https://res.cloudinary.com/adriantoddross/image/upload/v1528838462/Windfall_Browser_Window.png',
     title: 'Windfall',
     stack: 'Express.js, React JS, React Redux, Redux Forms, PostgresQL, knex.js, Heroku, Netlify',
-    description: 'Create a list of bills and see your net income.',
+    description: 'I created a simple budgeting app that lets users create list of bills and project their net income with PostgresQL, Redux Forms and DBeaver.',
     repo: 'https://github.com/thinkful-ei18/adrian-budget-client',
     link: 'https://windfall-app.netlify.com'
   }
