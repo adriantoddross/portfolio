a24e9d4b00b41bfb37bfe9c1313859affd228c7a
Adrian Ross <adriantoddross@gmail.com>
2019-02-11 17:26:49 -0500

    Credit friends of the web

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ export default function Footer(props) {
           <img className='footer-img' src="https://res.cloudinary.com/adriantoddross/image/upload/v1527785141/adrian-ross.png" alt="Adrian Ross"/>
         </div>
         <p>
-          Designed and developed by Adrian, <span className='attribution'>inspired by <a className='attribution' href="https://praneshravi.in/" target="_blank" rel="noopener noreferrer">Pranesh Ravi</a>.</span>
+          Designed and developed by Adrian, <span className='attribution'>inspired by <a className='attribution' href="https://praneshravi.in/" target="_blank" rel="noopener noreferrer">Pranesh Ravi</a> and <a className='attribution' href="https://friendsoftheweb.com/work/" target="_blank" rel="noopener noreferrer">Friends of the Web</a>.</span>
         </p>
         <p>
           Hosted on Netlify. Code is licensed under MIT and available on Github. All images and graphics are licensed under CC BY-SA.
