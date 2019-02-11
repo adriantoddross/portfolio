be468556985e683b18d61ce27fdbc8b3c878a895
Adrian Ross <adriantoddross@gmail.com>
2019-02-11 15:50:27 -0500

    Add https to linkedin link

--- src/components/Links.js
+++ src/components/Links.js
@@ -15,7 +15,7 @@ export default function Links(props) {
           <a className='' href="https://www.instagram.com/adriantoddross/" target="_blank" rel="noopener noreferrer">Instagram</a>
         </li>
         <li>
-          <a className='linkedin' href="www.linkedin.com/in/adriantoddross" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+          <a className='linkedin' href="https://www.linkedin.com/in/adriantoddross" target="_blank" rel="noopener noreferrer">LinkedIn</a>
         </li>
         <li>
           <a href="https://twitter.com/adriantoddross" target="_blank" rel="noopener noreferrer">Twitter</a>
