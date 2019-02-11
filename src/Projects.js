0b820e26dc9116d6a1354683358008e4dce9df04
Adrian Ross <adriantoddross@gmail.com>
2019-02-11 16:53:54 -0500

    Add optional category field

--- src/Projects.js
+++ src/Projects.js
@@ -1,5 +1,21 @@
 const projects = [
   {
+    image: 'https://res.cloudinary.com/adriantoddross/image/upload/v1549920429/goldmaneagle.png',
+    title: 'Goldman Eagle Advertising',
+    description: 'Goldman Eagle Advertising is a marketing and branding firm for small to mid-sized businesses. Squarespace was a great option to get an initial iteration of their site up and running and now that the site has its wings it\'s ready to soar!',
+    stack: 'Squarespace (content management system), CSS, Adobe Illustrator, Adobe Photoshop',
+    link: 'https://www.goldmaneagle.com/',
+    category: 'website'
+  },
+  {
+    image: 'https://res.cloudinary.com/adriantoddross/image/upload/v1549920576/myautocarrier.png',
+    title: 'MyAutoCarrier',
+    description: 'MyAutoCarrier is a car shipping company based out of Atlanta, GA. Unfortunately, I didn\'t get to see this project through. A miscommunication about the initial features and functionality of the site led to an unbelievable case of scope creep and I had to cease work on the site. Someday, I\'ll make a blog post about my mistakes throughout this process.',
+    stack: 'Squarespace (content management system), CSS',
+    link: 'https://www.myautocarrier.com/',
+    category: 'website'
+  },
+  {
     image: 'https://res.cloudinary.com/adriantoddross/image/upload/v1528837857/safeR_Browser_Window.png',
     title: 'SafeR',
     description: 'Our team of five created a neighborhood oriented social networking app to view and report local incidents in your community. We used Google Maps & Places APIs and also created a React Native app alongeside our web app.',
