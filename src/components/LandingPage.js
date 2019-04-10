5f22ab01d7a9eeab4137cf2aff9ecbec368d84d2
Adrian Ross <adriantoddross@gmail.com>
2019-04-09 23:01:57 -0400

    Fill a tag links

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -22,21 +22,21 @@ export default function LandingPage(props) {
       <div></div>
       <body>
         <div>
-          <h3>Preferred Tools</h3>
+          <h3>Selected Clients</h3>
           <ul>
-            <li>React</li>
-            <li>Node.js</li>
-            <li>Express.js</li>
+            <li><a href="https://www.icmgeorgia.org/" target="_blank" rel="noopener noreferrer">Interfaith Children's Movement</a></li>
+            <li><a href="https://www.goldmaneagle.com/" target="_blank" rel="noopener noreferrer">Goldman Eagle Advertising</a></li>
+            <li><a href="https://the-obsidian.sharetribe.com/" target="_blank" rel="noopener noreferrer">The Obsidian</a></li>
           </ul>
         </div>
         <div>
           <h3>Preferred Tools</h3>
           <ul>
-            <li><a href="http://" target="_blank" rel="noopener noreferrer"></a>React</li>
-            <li><a href="http://" target="_blank" rel="noopener noreferrer">Node.js</a></li>
-            <li><a href="http://" target="_blank" rel="noopener noreferrer">Express.js</a></li>
-            <li><a href="http://" target="_blank" rel="noopener noreferrer">MongoDB</a></li>
-            <li><a href="http://" target="_blank" rel="noopener noreferrer"></a>PostgresQL</li>
+            <li><a href="https://reactjs.org/" target="_blank" rel="noopener noreferrer">React</a></li>
+            <li><a href="https://nodejs.org/en/" target="_blank" rel="noopener noreferrer">Node.js</a></li>
+            <li><a href="https://expressjs.com/" target="_blank" rel="noopener noreferrer">Express.js</a></li>
+            <li><a href="https://www.mongodb.com/" target="_blank" rel="noopener noreferrer">MongoDB</a></li>
+            <li><a href="https://www.postgresql.org/" target="_blank" rel="noopener noreferrer">PostgresQL</a></li>
           </ul>
         </div>
       </body>
