97940e22554b677be40fe98e2ec1ede481d2cde6
Adrian Ross <adriantoddross@gmail.com>
2019-04-09 22:28:27 -0400

    Stub landing page

---
+++ src/components/LandingPage.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default function LandingPage(props) {
+  return (
+    <div>
+      <img src="" alt=""/>
+      <header>
+        <p><strong>Adrian Ross</strong> is a web developer and graphic designer in Atlanta, GA.</p>
+        <nav>
+          <ul>
+            <li>
+              <a href="">View Selected Work</a>
+            </li>
+            <li>
+              <a href="">Contact Me</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  );
+}
