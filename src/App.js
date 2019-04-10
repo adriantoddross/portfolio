7e68d268225caa7759231632ca6fae642d57370a
Adrian Ross <adriantoddross@gmail.com>
2019-04-09 22:35:55 -0400

    Remove CSS

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React, { Component } from 'react';
-import Header from './components/Header';
-import Portfolio from './components/Portfolio';
-import Footer from './components/Footer';
-import './App.css';
+import LandingPage from './components/LandingPage';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header/>
-        <Portfolio/>
-        <Footer/>
+        <LandingPage/>
       </div>
     );
   }
