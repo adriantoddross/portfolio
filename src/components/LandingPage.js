22ff0fdeaefb27727b4c22a9307477dc67e83833
Adrian Ross <adriantoddross@gmail.com>
2019-04-09 22:45:11 -0400

    Update all a tags attributes

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -3,20 +3,43 @@ import React from 'react';
 export default function LandingPage(props) {
   return (
     <div>
-      <img src="" alt=""/>
-      <header>
-        <p><strong>Adrian Ross</strong> is a web developer and graphic designer in Atlanta, GA.</p>
-        <nav>
+      <div>
+        <img src="" alt=""/>
+        <header>
+          <p><strong>Adrian Ross</strong> is a web developer and graphic designer in Atlanta, GA.</p>
+          <nav>
+            <ul>
+              <li>
+                <a href="http://" target="_blank" rel="noopener noreferrer">View Selected Work</a>
+              </li>
+              <li>
+                <a href="http://" target="_blank" rel="noopener noreferrer">Contact Me</a>
+              </li>
+            </ul>
+          </nav>
+        </header>
+      </div>
+      <div></div>
+      <body>
+        <div>
+          <h3>Preferred Tools</h3>
           <ul>
-            <li>
-              <a href="">View Selected Work</a>
-            </li>
-            <li>
-              <a href="">Contact Me</a>
-            </li>
+            <li>React</li>
+            <li>Node.js</li>
+            <li>Express.js</li>
           </ul>
-        </nav>
-      </header>
+        </div>
+        <div>
+          <h3>Preferred Tools</h3>
+          <ul>
+            <li><a href="http://" target="_blank" rel="noopener noreferrer"></a>React</li>
+            <li><a href="http://" target="_blank" rel="noopener noreferrer">Node.js</a></li>
+            <li><a href="http://" target="_blank" rel="noopener noreferrer">Express.js</a></li>
+            <li><a href="http://" target="_blank" rel="noopener noreferrer">MongoDB</a></li>
+            <li><a href="http://" target="_blank" rel="noopener noreferrer"></a>PostgresQL</li>
+          </ul>
+        </div>
+      </body>
     </div>
   );
 }
