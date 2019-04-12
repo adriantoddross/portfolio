b58f1d5ce74a572f66fbd4ece180f623904b61f1
Adrian Ross <adriantoddross@gmail.com>
2019-04-11 23:39:17 -0400

    Create html for packages

---
+++ src/components/Packages.js
@@ -0,0 +1,83 @@
+import React from 'react';
+
+export default function Packages() {
+  return (
+    <div>
+
+      <div>
+        <h2>Packages</h2>
+        <div></div>
+        <p>Packages offered for individuals and small businesses.</p>
+      </div>
+      <div>
+        <h3>Album Art Package</h3>
+        <p>$price</p>
+        <div></div>
+        <p>One-sided album art design converted into various formats for easy posting and sharing across social media. Also includes a 15-second shareable video to promote your new track.
+        </p>
+        <ul>
+          <li>Album art image (1080 x 1080 pixels)</li>
+          <li>Facebook profile picture (180 x 180 pixels)</li>
+          <li>Twitter profile picture (400 x 400 pixels)</li>
+          <li>Facebook cover photo header (860 x 360 pixels)</li>
+          <li>SoundCloud banner (2480 x 520 pixels)</li>
+          <li>Twitter profile header (1500 x 500 pixels)</li>
+          <li>15 second video clip (1500 x 1500 pixels)</li>
+        </ul>
+        
+        <div>
+          <h4>More details</h4>
+          <ul>
+            <li>1 design</li>
+            <li>Up to 3 revisions</li>
+            <li>Images delivered as JPEG and PNG files</li>
+            <li>Video clip delivered as an .mp4 file</li>
+            <li>Typically delivered within 4-8 business days</li>
+          </ul>
+          <button>I'm interested</button>
+        </div>
+      </div> 
+
+      <div>
+        <h3>Event promo package</h3>
+        <p>$price</p>
+        <div></div>
+        <p>One-sided event flyer design converted into various formats for easy posting and sharing across social media.
+        </p>
+        <ul>
+          <li>U.S. Letter (8.5 x 11 inches)</li>
+          <li>Handbill (5.5 by 8.5 inches)</li>
+          <li>Social media image (1080 x 1080 pixels)</li>
+          <li>Facebook cover photo header (860 x 360 pixels)</li>
+          <li>Twitter profile header (1500 x 500 pixels)</li>
+        </ul>
+        <div>
+          <h4>More details</h4>
+          <ul>
+            <li>1 design</li>
+            <li>Up to 3 revisions</li>
+            <li>Images delivered as JPEG, PDF and PNG files</li>
+            <li>Typically delivered within 2-4 business days</li>
+          </ul>
+          <button>I'm interested</button>
+        </div>
+
+        <div>
+          <h3>Landing Page</h3>
+          <p>$price</p>
+          <div></div>
+          <p>Single page website centered around a call-to-action like subscribing for a newsletter, ordering a new product, or registering for a webinar.
+          </p>
+          <h4>More details</h4>
+          <ul>
+            <li>1 website design</li>
+            <li>Unlimited revisions (within reason)</li>
+            <li>Images delivered as JPEG, PDF and PNG files</li>
+            <li>Typically completed within a month</li>
+          </ul>
+          <button>I'm interested</button>
+        </div>
+      </div>
+    </div>
+  );
+}
